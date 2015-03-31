55b75368927a5439a69fc8c37e755d331bcd5eb5
Gao Kai <gaok12@mails.tsinghua.edu.cn>
2015-03-31 22:41:42 +0800

    Fixed style errors in alto-model.
    Put some constants in commons.
    
    Change-Id: I3a2c94b5df1393c9beba061dd1e4e2fb3f28ebbc
    Signed-off-by: Gao Kai <gaok12@mails.tsinghua.edu.cn>

--- alto-northbound/src/main/java/org/opendaylight/alto/northbound/AltoNorthbound.java
+++ alto-northbound/src/main/java/org/opendaylight/alto/northbound/AltoNorthbound.java
@@ -7,12 +7,12 @@
  */
 package org.opendaylight.alto.northbound;
 
-import org.codehaus.jettison.json.JSONObject;
+import org.opendaylight.alto.commons.RFC7285MediaType;
 
+import org.codehaus.jettison.json.JSONObject;
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.slf4j.Logger;
@@ -23,57 +23,6 @@ public class AltoNorthbound {
 
     private static final Logger mLogger = LoggerFactory.getLogger(AltoNorthbound.class);
 
-    /* TODO
-     *
-     * Move them to AltoServiceAPI
-     *
-     * */
-
-    /** The media types defined in [RFC7285]
-     * */
-    public static final class AltoMediaType {
-        public static final String ALTO_DIRECTORY
-                                = "application/alto-directory+json";
-        public static final MediaType ALTO_DIRECTORY_TYPE
-                                = MediaType.valueOf(ALTO_DIRECTORY);
-        public static final String ALTO_NETWORKMAP
-                                = "application/alto-networkmap+json";
-        public static final MediaType ALTO_NETWORKMAP_TYPE
-                                = MediaType.valueOf(ALTO_NETWORKMAP);
-        public static final String ALTO_NETWORKMAP_FILTER
-                                = "application/alto-networkmapfilter+json";
-        public static final MediaType ALTO_NETWORKMAP_FILTER_TYPE
-                                = MediaType.valueOf(ALTO_NETWORKMAP_FILTER);
-        public static final String ALTO_COSTMAP
-                                = "application/alto-costmap+json";
-        public static final MediaType ALTO_COSTMAP_TYPE
-                                = MediaType.valueOf(ALTO_COSTMAP);
-        public static final String ALTO_COSTMAP_FILTER
-                                = "application/alto-costmapfilter+json";
-        public static final MediaType ALTO_COSTMAP_FILTER_TYPE
-                                = MediaType.valueOf(ALTO_COSTMAP_FILTER);
-        public static final String ALTO_ENDPOINT_PROP
-                                = "application/alto-endpointprop+json";
-        public static final MediaType ALTO_ENDPOINT_PROP_TYPE
-                                = MediaType.valueOf(ALTO_ENDPOINT_PROP);
-        public static final String ALTO_ENDPOINT_PROPPARAMS
-                                = "application/alto-endpointpropparams+json";
-        public static final MediaType ALTO_ENDPOINT_PROPPARAMS_TYPE
-                                = MediaType.valueOf(ALTO_ENDPOINT_PROPPARAMS);
-        public static final String ALTO_ENDPOINT_COST
-                                = "application/alto-endpointcost+json";
-        public static final MediaType ALTO_ENDPOINT_COST_TYPE
-                                = MediaType.valueOf(ALTO_ENDPOINT_COST);
-        public static final String ALTO_ENDPOINT_COSTPARAMS
-                                = "application/alto-endpointcostparams+json";
-        public static final MediaType ALTO_ENDPOINT_COSTPARAMS_TYPE
-                                = MediaType.valueOf(ALTO_ENDPOINT_COSTPARAMS);
-        public static final String ALTO_ERROR
-                                = "application/alto-error+json";
-        public static final MediaType ALTO_ERROR_TYPE
-                                = MediaType.valueOf(ALTO_ERROR);
-    }
-
     public class AltoError {
         public static final String E_SYNTAX = "syntax-error";
         public static final String E_MISSING_FIELD = "missing-field";
@@ -82,14 +31,14 @@ public class AltoNorthbound {
     }
 
     @GET
-    @Produces({AltoMediaType.ALTO_DIRECTORY, AltoMediaType.ALTO_ERROR})
+    @Produces({RFC7285MediaType.ALTO_DIRECTORY, RFC7285MediaType.ALTO_ERROR})
     public Response retrieveIRD() {
         JSONObject ird = new JSONObject();
         try {
             ird.put("test", "ok");
         } catch (Exception e) {
         }
-        return Response.ok(ird.toString(), AltoMediaType.ALTO_ERROR).build();
+        return Response.ok(ird.toString(), RFC7285MediaType.ALTO_ERROR).build();
     }
 
     @Path("/hello")
